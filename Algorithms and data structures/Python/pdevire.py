a56ba2dca4c7748422fb2fcd361835df03c64836
Rezepin Nikita <rezepinn@yandex.ru>
2021-06-11 16:24:22 +0300

    Переписал прозводную полинома под функцию посимвольного ввода

--- Algorithms and data structures/Python/pdevire.py
+++ Algorithms and data structures/Python/pdevire.py
@@ -1,14 +1,13 @@
 from math import factorial
+from read_to_space import read_to_space
 
 
 def pdevire():
-    n, k, x0 = map(int, input().split())
-    an = input().split()
+    n, k, x0 = int(read_to_space()), int(read_to_space()), int(read_to_space())
     sum = 0
     for i in range(n-k+1):
-        sum += factorial(n-i) / factorial(n-i-k) * (int(an[i])) * (x0 ** (n-i))
-    print(int(sum))
-    return
+        sum += factorial(n-i) / factorial(n-i-k) * (int(read_to_space())) * (x0 ** (n-i))
+    return int(sum)
 
 
-pdevire()
+print(pdevire())
