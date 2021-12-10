94d66a57a991e9872a6b3dad67715aa7541accc4
not-Whale <rezepinn@yandex.ru>
2021-12-11 02:57:05 +0300

    Scripted auto-commit on change (2021-12-11 02:57:05) by gitwatch.sh

--- Development of Parallel and Distributed Programs/lab_2/src/main/java/FlightsStatsApp.java
+++ Development of Parallel and Distributed Programs/lab_2/src/main/java/FlightsStatsApp.java
@@ -6,7 +6,7 @@ public class FlightsStatsApp {
         job.setJarByClass(FlightsStatsApp.class);
         job.setJobName("Reduce side join App");
 
-        // 
+        // задание путей файлов ввода
         MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, FlightsMapper.class);
         MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, AirportsMapper.class);
     }
