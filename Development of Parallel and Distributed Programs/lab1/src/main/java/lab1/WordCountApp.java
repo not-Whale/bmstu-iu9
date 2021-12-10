59553d7f2915578f494feb016c0195f572300a1f
not-Whale <rezepinn@yandex.ru>
2021-12-10 05:03:28 +0300

    Scripted auto-commit on change (2021-12-10 05:03:28) by gitwatch.sh

--- Development of Parallel and Distributed Programs/lab1/src/main/java/lab1/WordCountApp.java
+++ Development of Parallel and Distributed Programs/lab1/src/main/java/lab1/WordCountApp.java
@@ -20,7 +20,7 @@ public class WordCountApp {
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);
         job.setNumReduceTasks(2);
-        
+
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
