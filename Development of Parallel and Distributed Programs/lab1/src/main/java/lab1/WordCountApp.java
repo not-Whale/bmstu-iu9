62fb89bfd19779165194fb7a0eca09078c783822
not-Whale <rezepinn@yandex.ru>
2021-12-10 05:21:21 +0300

    Scripted auto-commit on change (2021-12-10 05:21:21) by gitwatch.sh

--- Development of Parallel and Distributed Programs/lab1/src/main/java/lab1/WordCountApp.java
+++ Development of Parallel and Distributed Programs/lab1/src/main/java/lab1/WordCountApp.java
@@ -1,8 +1,6 @@
-package lab1;
-
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
