4fe3c8be15c3dc00736d4a06edbfc54c79b3d609
not-Whale <rezepinn@yandex.ru>
2021-12-10 18:42:19 +0300

    Scripted auto-commit on change (2021-12-10 18:42:18) by gitwatch.sh

--- Development of Parallel and Distributed Programs/lab_1/src/main/java/lw1/WordMapper.java
+++ Development of Parallel and Distributed Programs/lab_1/src/main/java/lw1/WordMapper.java
@@ -11,7 +11,9 @@ public class WordMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         // <получаем строку из value, удаляем все спецсимволы, переводим в lowercase, разбиваем на слова и каждое слово пишем в контекст с счетчиком 1
-        // в контекст пишется пара — Text и IntWritable >
+        // в контекст пишется пара — Text и IntWritable>
+
+        String inputString = value.toString();
 
         
     }
