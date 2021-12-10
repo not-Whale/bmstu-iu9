feb249075ebd49d910924384a59ea093d64068b8
not-Whale <rezepinn@yandex.ru>
2021-12-10 19:05:25 +0300

    Scripted auto-commit on change (2021-12-10 19:05:25) by gitwatch.sh

--- Development of Parallel and Distributed Programs/lab_1/src/main/java/lw1/WordReducer.java
+++ Development of Parallel and Distributed Programs/lab_1/src/main/java/lw1/WordReducer.java
@@ -10,16 +10,13 @@ import java.io.IOException;
 public class WordReducer extends Reducer<Text, IntWritable, Text, LongWritable> {
     @Override
     protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
-        // <считаем количество записей в итераторе и генерируем запись в контекст
-        // В контекст пишется пара — Text и LongWritable>
-
         // считаем количество записей в итераторе
         long count = 0;
         for (IntWritable value : values) {
             count++;
         }
 
-        // записываем в контекст 
+        // записываем в контекст
         context.write(key, new LongWritable(count));
     }
 }
