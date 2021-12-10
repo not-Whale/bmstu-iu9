bdba8f4edf580625e039f10e2f1234f1b0cd6ff1
not-Whale <rezepinn@yandex.ru>
2021-12-10 15:23:55 +0300

    Почему-то снова два файла не добавились в коммит

--- Development of Parallel and Distributed Programs/lab1/src/main/java/lab1/WordReducer.java
+++ Development of Parallel and Distributed Programs/lab1/src/main/java/lab1/WordReducer.java
@@ -10,10 +10,6 @@ import java.io.IOException;
 public class WordReducer extends Reducer<Text, IntWritable, Text, LongWritable> {
     @Override
     protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
-       // <считаем количество записей в итераторе и
-        // генерируем запись в контекст
-       // В контекст пишется пара — Text и LongWritable>
-
         // считаем количество записей в итераторе
         long count = 0;
         for (IntWritable value : values) {
