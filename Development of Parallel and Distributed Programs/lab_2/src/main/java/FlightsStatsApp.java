73cb0d3ff8c04715c18203600f1272569e969f8d
not-Whale <rezepinn@yandex.ru>
2021-12-11 02:58:30 +0300

    Scripted auto-commit on change (2021-12-11 02:58:30) by gitwatch.sh

--- Development of Parallel and Distributed Programs/lab_2/src/main/java/FlightsStatsApp.java
+++ Development of Parallel and Distributed Programs/lab_2/src/main/java/FlightsStatsApp.java
@@ -1,6 +1,7 @@
 public class FlightsStatsApp {
     public static void main(Stringp[] args) throws Exception {
         // проверка на правильность ввода
+        // формат ввода 
 
         Job job = Job.getInstance();
         job.setJarByClass(FlightsStatsApp.class);
@@ -9,6 +10,6 @@ public class FlightsStatsApp {
         // задание путей файлов ввода и вывода
         MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, FlightsMapper.class);
         MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, AirportsMapper.class);
-        FileOutputFormat
+        FileOutputFormat.addInputPath(job, new Path(args[2]));
     }
 }
