a8702d317c4a040a622c25deb8f31db79ced2305
not-Whale <rezepinn@yandex.ru>
2021-12-10 18:11:09 +0300

    Scripted auto-commit on change (2021-12-10 18:11:09) by gitwatch.sh

--- Development of Parallel and Distributed Programs/lab_1/src/main/java/lw1/WordMapper.java
+++ Development of Parallel and Distributed Programs/lab_1/src/main/java/lw1/WordMapper.java
@@ -1,4 +1,18 @@
 package lw1;
 
-public class WordMapper {
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+public class WordMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        // <получаем строку из value, удаляем все спецсимволы, переводим в lowercase, разбиваем на слова и каждое слово пишем в контекст с счетчиком 1
+        // в контекст пишется пара — Text и IntWritable >
+
+        
+    }
 }
