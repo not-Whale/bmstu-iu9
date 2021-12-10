ffb2850b17c2a0449999b63993c3f6d407f8d099
not-Whale <rezepinn@yandex.ru>
2021-12-10 19:05:08 +0300

    Scripted auto-commit on change (2021-12-10 19:05:08) by gitwatch.sh

--- Development of Parallel and Distributed Programs/lab_1/src/main/java/lw1/WordReducer.java
+++ Development of Parallel and Distributed Programs/lab_1/src/main/java/lw1/WordReducer.java
@@ -1,4 +1,25 @@
 package lw1;
 
-public class WordReducer {
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
+public class WordReducer extends Reducer<Text, IntWritable, Text, LongWritable> {
+    @Override
+    protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+        // <считаем количество записей в итераторе и генерируем запись в контекст
+        // В контекст пишется пара — Text и LongWritable>
+
+        // считаем количество записей в итераторе
+        long count = 0;
+        for (IntWritable value : values) {
+            count++;
+        }
+
+        // записываем в контекст 
+        context.write(key, new LongWritable(count));
+    }
 }
