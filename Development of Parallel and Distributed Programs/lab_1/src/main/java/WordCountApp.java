290686ecbba18708de36a20ba0b7faba89c3b69a
not-Whale <rezepinn@yandex.ru>
2021-12-10 19:53:50 +0300

    Изменил структуру файловой системы, чтобы не ругался хадуп

--- Development of Parallel and Distributed Programs/lab_1/src/main/java/WordCountApp.java
+++ Development of Parallel and Distributed Programs/lab_1/src/main/java/WordCountApp.java
@@ -1,5 +1,3 @@
-package lw1;
-
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.IntWritable;
--- Development of Parallel and Distributed Programs/lab_1/src/main/java/WordMapper.java
+++ Development of Parallel and Distributed Programs/lab_1/src/main/java/WordMapper.java
@@ -1,5 +1,3 @@
-package lw1;
-
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
@@ -19,18 +17,20 @@ public class WordMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
         // сначала для прямой речи
         inputString = inputString.replaceAll("[—\\s]", " ");
         inputString = inputString.replaceAll("[-\\s]", " ");
+
         // потом отдельно точки (для случая разделения цифр, например: "40.000"
         // или для сокращений - "Н. Э. Баумана")
-        inputString = inputString.replaceAll(".", "");
+        // inputString = inputString.replaceAll(".", "");
+        
         // далее все специальные символы, кроме ' для французский слов
         // и "картавости", а также дефисов внутри слов (например, "красно-синий")
-        inputString = inputString.replaceAll("[\\W&&[^-']]", " ");
+        inputString = inputString.replaceAll("[\\W&&[^-']]", "");
         // "схлопываем" пробелы и удаляем их в начале и конце файла
         inputString = inputString.replaceAll("[ +]", " ");
         inputString = inputString.trim();
 
         // разделение на слова, запись в массив
-        String[] words = inputString.split("\\s");
+        String[] words = inputString.split(" ");
 
         // запись слов в контекс с счетчиком 1
         for (String word : words) {
