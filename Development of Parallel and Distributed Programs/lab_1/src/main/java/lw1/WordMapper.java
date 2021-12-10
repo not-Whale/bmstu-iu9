bfd680120e6923d2f7902811f16c1268d0958b30
not-Whale <rezepinn@yandex.ru>
2021-12-10 19:09:11 +0300

    Scripted auto-commit on change (2021-12-10 19:09:11) by gitwatch.sh

--- Development of Parallel and Distributed Programs/lab_1/src/main/java/lw1/WordMapper.java
+++ Development of Parallel and Distributed Programs/lab_1/src/main/java/lw1/WordMapper.java
@@ -10,15 +10,22 @@ import java.io.IOException;
 public class WordMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-        // получаем строку из value 
+        // получаем строку из value
         String inputString = value.toString();
 
-        //
+        // удаляем все спецсимволы
+        // сначала для прямой речи
         inputString = inputString.replaceAll("[—\\s]", " ");
         inputString = inputString.replaceAll("[-\\s]", " ");
+        // потом отдельно точки (для случая разделения цифр, например: "40.000"
+        // или для сокращений - "Н. Э. Баумана")
         inputString = inputString.replaceAll(".", "");
+        // далее все специальные символы, кроме ' для французский слов
+        // и "картавости", а также дефисов внутри слов (например, "красно-синий")
         inputString = inputString.replaceAll("[\\W&&[^-']]", " ");
+        // "схлопываем" пробелы и удаляем их в начале и конце файла
         inputString = inputString.replaceAll("[ +]", " ");
+        inputString = inputString.trim();
 
         String[] words = inputString.split("\\s");
 
