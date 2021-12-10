b835958b071ca47c0798f66026f3c20d0ca7b0d9
not-Whale <rezepinn@yandex.ru>
2021-12-10 18:55:27 +0300

    Scripted auto-commit on change (2021-12-10 18:55:27) by gitwatch.sh

--- Development of Parallel and Distributed Programs/lab_1/src/main/java/lw1/WordMapper.java
+++ Development of Parallel and Distributed Programs/lab_1/src/main/java/lw1/WordMapper.java
@@ -10,7 +10,8 @@ import java.io.IOException;
 public class WordMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-        // <получаем строку из value, удаляем все спецсимволы, переводим в lowercase, разбиваем на слова и каждое слово пишем в контекст с счетчиком 1
+        // <получаем строку из value, удаляем все спецсимволы, переводим в lowercase, разбиваем
+        // на слова и каждое слово пишем в контекст с счетчиком 1
         // в контекст пишется пара — Text и IntWritable>
 
         String inputString = value.toString();
@@ -19,5 +20,11 @@ public class WordMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
         inputString = inputString.replaceAll("[—\\s]", " ");
         inputString = inputString.replaceAll("[\\W&&[-']]", " ");
         inputString = inputString.replaceAll("[ +]", " ");
+
+        String[] words = inputString.split(" ");
+
+        for (String word : words) {
+            context.write(new Text(word), new IntWritable(1));
+        }
     }
 }
