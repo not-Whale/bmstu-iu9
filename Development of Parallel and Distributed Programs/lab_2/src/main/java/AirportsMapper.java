4e1c384a67c4d84a7b8a6ff3a9a1391e4667fa33
not-Whale <rezepinn@yandex.ru>
2021-12-11 05:57:45 +0300

    Scripted auto-commit on change (2021-12-11 05:57:45) by gitwatch.sh

--- Development of Parallel and Distributed Programs/lab_2/src/main/java/AirportsMapper.java
+++ Development of Parallel and Distributed Programs/lab_2/src/main/java/AirportsMapper.java
@@ -15,12 +15,7 @@ public class AirportsMapper extends Mapper<LongWritable, Text, AnFWritableCompar
         String[] row = value.toString().split(SEPARATOR);
 
         if (key.get() > 0) {
-            String destinationAirport = row[DEST_AIRPORT_ID];
-            String flightDelay = row[FLIGHT_DELAY];
-            boolean cancelled = Float.parseFloat(row[CANCELLED_STATUS]) == CANCELLED;
-            if (!cancelled && !flightDelay.isEmpty()) {
-                context.write(new AnFWritableComparable(Integer.parseInt(destinationAirport), INDICATOR), new Text(flightDelay));
-            }
+            String airportCode = row[AIRPORT_CODE];
         }
     }
 }
