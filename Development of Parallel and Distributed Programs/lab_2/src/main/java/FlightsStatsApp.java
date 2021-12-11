dbd3d78604b1837be1e91811ec67324e18a01439
not-Whale <rezepinn@yandex.ru>
2021-12-11 03:06:43 +0300

    Scripted auto-commit on change (2021-12-11 03:06:43) by gitwatch.sh

--- Development of Parallel and Distributed Programs/lab_2/src/main/java/FlightsStatsApp.java
+++ Development of Parallel and Distributed Programs/lab_2/src/main/java/FlightsStatsApp.java
@@ -2,6 +2,7 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class FlightsStatsApp {
     public static void main(Stringp[] args) throws Exception {
