a7663aa4a4b0361595e549844fa17d3d23de5b2d
not-Whale <rezepinn@yandex.ru>
2021-12-11 03:46:10 +0300

    Scripted auto-commit on change (2021-12-11 03:46:10) by gitwatch.sh

--- Development of Parallel and Distributed Programs/lab_2/src/main/java/AirportsPartitioner.java
+++ Development of Parallel and Distributed Programs/lab_2/src/main/java/AirportsPartitioner.java
@@ -1,2 +1,11 @@
-public class AirportsPartitioner {
+import org.apache.hadoop.mapreduce.Partitioner;
+
+import javax.xml.soap.Text;
+
+public class AirportsPartitioner extends Partitioner<AnFWritableComparable, Text> {
+
+    @Override
+    public int getPartition(AnFWritableComparable anFWritableComparable, Text text, int i) {
+        return 0;
+    }
 }
