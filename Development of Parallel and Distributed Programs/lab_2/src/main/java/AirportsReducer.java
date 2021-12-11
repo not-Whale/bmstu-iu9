973051696a775f1312e535ab6580ec3a75eb4b89
not-Whale <rezepinn@yandex.ru>
2021-12-11 06:25:06 +0300

    Scripted auto-commit on change (2021-12-11 06:25:06) by gitwatch.sh

--- Development of Parallel and Distributed Programs/lab_2/src/main/java/AirportsReducer.java
+++ Development of Parallel and Distributed Programs/lab_2/src/main/java/AirportsReducer.java
@@ -4,6 +4,8 @@ import org.apache.hadoop.mapreduce.Reducer;
 
 public class AirportsReducer extends Reducer<AnFWritableComparable, Text, Text, Text> {
     protected void reduce(AnFWritableComparable key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
-        
+        float minDelay = Float.MAX_VALUE;
+        float maxDelay = Float.MIN_VALUE;
+        float 
     }
 }
