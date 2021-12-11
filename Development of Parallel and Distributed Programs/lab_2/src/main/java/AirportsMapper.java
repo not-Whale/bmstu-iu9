b8d106cf49a8e7ba6b2137f43317255dee4e0e9e
not-Whale <rezepinn@yandex.ru>
2021-12-11 03:44:52 +0300

    Scripted auto-commit on change (2021-12-11 03:44:52) by gitwatch.sh

--- Development of Parallel and Distributed Programs/lab_2/src/main/java/AirportsMapper.java
+++ Development of Parallel and Distributed Programs/lab_2/src/main/java/AirportsMapper.java
@@ -1,2 +1,7 @@
-public class AirportsMapper {
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class AirportsMapper extends Mapper<LongWritable, Text, AnFWritableComparable, Text> {
+
 }
