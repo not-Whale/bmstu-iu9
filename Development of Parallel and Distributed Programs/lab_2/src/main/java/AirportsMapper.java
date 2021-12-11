978840911a8b49ee4ada7c1160b7066af8392ef6
not-Whale <rezepinn@yandex.ru>
2021-12-11 05:55:20 +0300

    Scripted auto-commit on change (2021-12-11 05:55:20) by gitwatch.sh

--- Development of Parallel and Distributed Programs/lab_2/src/main/java/AirportsMapper.java
+++ Development of Parallel and Distributed Programs/lab_2/src/main/java/AirportsMapper.java
@@ -2,6 +2,24 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.mapreduce.Mapper;
 
-public class AirportsMapper extends Mapper<LongWritable, Text, AnFWritableComparable, Text> {
+import java.io.IOException;
 
+public class AirportsMapper extends Mapper<LongWritable, Text, AnFWritableComparable, Text> {
+    public static final String SEPARATOR = "\",\"";
+    public static final int INDICATOR = 0;
+    public static final int AIRPORT_ID
+
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        String[] row = value.toString().split(SEPARATOR);
+
+        if (key.get() > 0) {
+            String destinationAirport = row[DEST_AIRPORT_ID];
+            String flightDelay = row[FLIGHT_DELAY];
+            boolean cancelled = Float.parseFloat(row[CANCELLED_STATUS]) == CANCELLED;
+            if (!cancelled && !flightDelay.isEmpty()) {
+                context.write(new AnFWritableComparable(Integer.parseInt(destinationAirport), INDICATOR), new Text(flightDelay));
+            }
+        }
+    }
 }
