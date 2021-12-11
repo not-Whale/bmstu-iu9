d0c222114742f32d115013ac479223d9e236ffa9
not-Whale <rezepinn@yandex.ru>
2021-12-11 03:29:11 +0300

    Scripted auto-commit on change (2021-12-11 03:29:11) by gitwatch.sh

--- Development of Parallel and Distributed Programs/lab_2/src/main/java/FlightsStatsApp.java
+++ Development of Parallel and Distributed Programs/lab_2/src/main/java/FlightsStatsApp.java
@@ -1,3 +1,6 @@
+package lw2;
+
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
