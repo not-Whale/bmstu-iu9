756686467053a98ed2f86b2bc33824ce978fbad4
not-Whale <rezepinn@yandex.ru>
2021-12-11 07:33:31 +0300

    Scripted auto-commit on change (2021-12-11 07:33:31) by gitwatch.sh

--- Development of Parallel and Distributed Programs/lab_2/src/main/java/AirportsMapper.java
+++ Development of Parallel and Distributed Programs/lab_2/src/main/java/AirportsMapper.java
@@ -15,7 +15,7 @@ public class AirportsMapper extends Mapper<LongWritable, Text, AnFWritableCompar
         String[] row = value.toString().split(SEPARATOR);
 
         if (key.get() > 0) {
-            String airportCodeStr = row[AIRPORT_CODE].replaceAll("\"", " ").trim();
+            String airportCodeStr = row[AIRPORT_CODE].replaceAll("\"", "").trim();
             int airportCode = Integer.parseInt(airportCodeStr);
             String airportDescription = row[AIRPORT_DESCRIPTION].replaceAll("\"", "");
             context.write(new AnFWritableComparable(airportCode, INDICATOR), new Text(airportDescription));
