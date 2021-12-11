1165ad6c5c15fab84982ff2483225ca7a33d1174
not-Whale <rezepinn@yandex.ru>
2021-12-11 06:59:30 +0300

    Scripted auto-commit on change (2021-12-11 06:59:30) by gitwatch.sh

--- Development of Parallel and Distributed Programs/lab_2/src/main/java/AirportsMapper.java
+++ Development of Parallel and Distributed Programs/lab_2/src/main/java/AirportsMapper.java
@@ -5,7 +5,7 @@ import org.apache.hadoop.mapreduce.Mapper;
 import java.io.IOException;
 
 public class AirportsMapper extends Mapper<LongWritable, Text, AnFWritableComparable, Text> {
-    public static final String SEPARATOR = ",";
+    public static final String SEPARATOR = "\",\"";
     public static final int INDICATOR = 0;
     public static final int AIRPORT_CODE = 0;
     public static final int AIRPORT_DESCRIPTION = 1;
