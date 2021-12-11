6cccd3d3e6a90069a5866ce7be0b22d7604136b7
not-Whale <rezepinn@yandex.ru>
2021-12-11 03:45:28 +0300

    Scripted auto-commit on change (2021-12-11 03:45:28) by gitwatch.sh

--- Development of Parallel and Distributed Programs/lab_2/src/main/java/AnFWritableComparable.java
+++ Development of Parallel and Distributed Programs/lab_2/src/main/java/AnFWritableComparable.java
@@ -1,2 +1,23 @@
-public class AnFWritableComparable {
+import org.apache.hadoop.io.WritableComparable;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+public class AnFWritableComparable implements WritableComparable<AnFWritableComparable> {
+
+    @Override
+    public int compareTo(AnFWritableComparable o) {
+        return 0;
+    }
+
+    @Override
+    public void write(DataOutput dataOutput) throws IOException {
+
+    }
+
+    @Override
+    public void readFields(DataInput dataInput) throws IOException {
+
+    }
 }
