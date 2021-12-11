64918bdcc7de96725e089771f0837c00c1cf4aa8
not-Whale <rezepinn@yandex.ru>
2021-12-11 05:37:48 +0300

    Scripted auto-commit on change (2021-12-11 05:37:47) by gitwatch.sh

--- Development of Parallel and Distributed Programs/lab_2/src/main/java/FlightsMapper.java
+++ Development of Parallel and Distributed Programs/lab_2/src/main/java/FlightsMapper.java
@@ -5,8 +5,11 @@ import org.apache.hadoop.mapreduce.Mapper;
 import java.io.IOException;
 
 public class FlightsMapper extends Mapper<LongWritable, Text, AnFWritableComparable, Text> {
+    public static final String SEPARATOR = ",";
+    
+
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-        
+
     }
 }
