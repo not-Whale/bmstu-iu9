cb97f397a7a03232cd5524fd485a410b3dc9cac6
not-Whale <rezepinn@yandex.ru>
2021-12-11 06:19:30 +0300

    Scripted auto-commit on change (2021-12-11 06:19:30) by gitwatch.sh

--- Development of Parallel and Distributed Programs/lab_2/src/main/java/AirportsGroupingComparator.java
+++ Development of Parallel and Distributed Programs/lab_2/src/main/java/AirportsGroupingComparator.java
@@ -1,6 +1,9 @@
+import org.apache.hadoop.io.WritableComparable;
 import org.apache.hadoop.io.WritableComparator;
 
 public class AirportsGroupingComparator extends WritableComparator {
     @Override
-    public int
+    public int compare(WritableComparable a, WritableComparable b) {
+        
+    }
 }
