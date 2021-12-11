2c6b1f6fb7d1ac3d1cf2eab222e9525b43eee684
not-Whale <rezepinn@yandex.ru>
2021-12-11 05:45:16 +0300

    Scripted auto-commit on change (2021-12-11 05:45:16) by gitwatch.sh

--- Development of Parallel and Distributed Programs/lab_2/src/main/java/FlightsMapper.java
+++ Development of Parallel and Distributed Programs/lab_2/src/main/java/FlightsMapper.java
@@ -7,10 +7,19 @@ import java.io.IOException;
 public class FlightsMapper extends Mapper<LongWritable, Text, AnFWritableComparable, Text> {
     public static final String SEPARATOR = ",";
     public static final int INDICATOR = 1;
-    
+    public static final float CANCELLED = 1.0f;
+    public static final int CANCELLED_STATUS = 19;
+    public static final int DEST_AIRPORT_ID = 14;
+    public static final int FLIGHT_DELAY = 18;
 
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        String[] row = value.toString().split(SEPARATOR);
 
+        if (key.get() > 0) {
+            String destinationAirport = row[DEST_AIRPORT_ID];
+            String flightDelay = row[FLIGHT_DELAY];
+            
+        }
     }
 }
