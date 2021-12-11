c5d367ee3a5955da2251262e0bfee35fc281cb1a
not-Whale <rezepinn@yandex.ru>
2021-12-11 03:09:04 +0300

    Scripted auto-commit on change (2021-12-11 03:09:04) by gitwatch.sh

--- Development of Parallel and Distributed Programs/lab_2/src/main/java/FlightsStatsApp.java
+++ Development of Parallel and Distributed Programs/lab_2/src/main/java/FlightsStatsApp.java
@@ -22,6 +22,8 @@ public class FlightsStatsApp {
         MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, AirportsMapper.class);
         FileOutputFormat.addInputPath(job, new Path(args[2]));
 
+        
+
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
