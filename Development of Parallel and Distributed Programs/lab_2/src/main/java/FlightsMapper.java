1193e157dfbe3749afc42e30672e0032caf433db
not-Whale <rezepinn@yandex.ru>
2021-12-11 03:43:56 +0300

    Scripted auto-commit on change (2021-12-11 03:43:56) by gitwatch.sh

--- Development of Parallel and Distributed Programs/lab_2/src/main/java/FlightsMapper.java
+++ Development of Parallel and Distributed Programs/lab_2/src/main/java/FlightsMapper.java
@@ -1,4 +1,7 @@
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.mapreduce.Mapper;
 
-public class FlightsMapper extends Mapper<LongWritable, Text, AnFWritableComparable> {
+public class FlightsMapper extends Mapper<LongWritable, Text, AnFWritableComparable, Text> {
+
 }
