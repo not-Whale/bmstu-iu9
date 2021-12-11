a3df59050848efeb96e28c6b27eb59c68eda9d7b
not-Whale <rezepinn@yandex.ru>
2021-12-11 03:47:52 +0300

    Scripted auto-commit on change (2021-12-11 03:47:52) by gitwatch.sh

--- Development of Parallel and Distributed Programs/lab_2/src/main/java/AirportsReducer.java
+++ Development of Parallel and Distributed Programs/lab_2/src/main/java/AirportsReducer.java
@@ -1,2 +1,6 @@
-public class AirportsReducer {
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class AirportsReducer extends Reducer<AnFWritableComparable, Text, Text, Text> {
+
 }
