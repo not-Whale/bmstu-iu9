2a1e1105e8b0b8f9ed4dd97b24c6e2ed06056a3c
not-Whale <rezepinn@yandex.ru>
2021-12-18 00:55:15 +0300

    Scripted auto-commit on change (2021-12-18 00:55:15) by gitwatch.sh

--- Development of Parallel and Distributed Programs/lab_3/src/main/java/FlightsDelay.java
+++ Development of Parallel and Distributed Programs/lab_3/src/main/java/FlightsDelay.java
@@ -3,4 +3,9 @@ import java.io.Serializable;
 public class FlightsDelay implements Serializable {
     private float delayedFlightsNumber;
     private float cancelledFlightNumber;
+    private float maxDelay;
+    private final int flightsNumber;
+
+    public FlightsDelay() {
+    }
 }
