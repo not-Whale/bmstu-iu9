9589f56cc87c6a8af783878d3ca5c2b48a461626
not-Whale <rezepinn@yandex.ru>
2021-12-18 02:03:00 +0300

    Scripted auto-commit on change (2021-12-18 02:03:00) by gitwatch.sh

--- Development of Parallel and Distributed Programs/lab_3/src/main/java/AirportsFlightsStats.java
+++ Development of Parallel and Distributed Programs/lab_3/src/main/java/AirportsFlightsStats.java
@@ -1,7 +1,21 @@
+import scala.Tuple2;
+
+import java.util.Map;
+
 public class AirportsFlightsStats extends DelaysStats {
     private final String departureAirportDescription;
     private final String destinationAirportDescription;
 
     public AirportsFlightsStats(Tuple2<String, String> airportDescriptions,
-                                DelaysStats delaysStats, Map<String, Sting> a)
+                                DelaysStats delaysStats, Map<String, String> airportDescription) {
+        super(
+                delaysStats.getMaxDelay(),
+                delaysStats.getFlightsNumber(),
+                delaysStats.getDelayedFlightsCount(),
+                delaysStats.getCancelledFlightsCount()
+        );
+
+        this.departureAirportDescription = airportDescription.get(airportDescriptions._1());
+        this.destinationAirportDescription = airportDescription.get(airportDescriptions._2());
+    }
 }
