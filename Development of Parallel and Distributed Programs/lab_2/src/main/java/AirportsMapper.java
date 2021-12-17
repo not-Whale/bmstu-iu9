0017cf22161b68fff16ddce5b9b3bb37c6200d3d
not-Whale <rezepinn@yandex.ru>
2021-12-17 20:23:53 +0300

    Scripted auto-commit on change (2021-12-17 20:23:53) by gitwatch.sh

--- Development of Parallel and Distributed Programs/lab_2/src/main/java/AirportsMapper.java
+++ Development of Parallel and Distributed Programs/lab_2/src/main/java/AirportsMapper.java
@@ -1,3 +1,4 @@
+import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.mapreduce.Mapper;
@@ -5,20 +6,30 @@ import org.apache.hadoop.mapreduce.Mapper;
 import java.io.IOException;
 
 public class AirportsMapper extends Mapper<LongWritable, Text, AnFWritableComparable, Text> {
-    public static final String SEPARATOR = ",";
-    public static final int INDICATOR = 0;
-    public static final int AIRPORT_CODE = 0;
-    public static final int AIRPORT_DESCRIPTION = 1;
+    private static final String SEPARATOR = ",";
+    private static final int INDICATOR = 0;
+    private static final int AIRPORT_CODE = 0;
+    private static final int AIRPORT_DESCRIPTION = 1;
 
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         String[] row = value.toString().split(SEPARATOR);
 
         if (key.get() > 0) {
-            String airportCodeStr = row[AIRPORT_CODE].replaceAll("\"", "").trim();
-            int airportCode = Integer.parseInt(airportCodeStr);
-            String airportDescription = row[AIRPORT_DESCRIPTION].replaceAll("\"", "").trim();
-            context.write(new AnFWritableComparable(airportCode, INDICATOR), new Text(airportDescription));
+            String airportCodeString = row[AIRPORT_CODE]
+                    .replaceAll("\"", "")
+                    .trim();
+            String airportDescription = row[AIRPORT_DESCRIPTION]
+                    .replaceAll("\"", "")
+                    .trim();
+            int airportCode = Integer.parseInt(airportCodeString);
+            context.write(
+                    new AnFWritableComparable(
+                            new IntWritable(airportCode),
+                            new IntWritable(INDICATOR)
+                    ),
+                    new Text(airportDescription)
+            );
         }
     }
 }
