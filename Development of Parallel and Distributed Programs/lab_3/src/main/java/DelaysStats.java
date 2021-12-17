cbd96fe85cf15279d65f5100a4a4c753a3e25cab
not-Whale <rezepinn@yandex.ru>
2021-12-18 00:56:39 +0300

    Scripted auto-commit on change (2021-12-18 00:56:38) by gitwatch.sh

--- Development of Parallel and Distributed Programs/lab_3/src/main/java/DelaysStats.java
+++ Development of Parallel and Distributed Programs/lab_3/src/main/java/DelaysStats.java
@@ -1,4 +1,11 @@
 import java.io.Serializable;
 
 public class DelaysStats implements Serializable {
+    private float delayedFlightsNumber;
+    private float cancelledFlightNumber;
+    private float maxDelay;
+    private final int flightsNumber;
+
+    public DelaysStats() {
+    }
 }
--- Development of Parallel and Distributed Programs/lab_3/src/main/java/FlightsDelay.java
+++ Development of Parallel and Distributed Programs/lab_3/src/main/java/FlightsDelay.java
@@ -1,12 +1,6 @@
 import java.io.Serializable;
 
 public class FlightsDelay implements Serializable {
-    private float delayedFlightsNumber;
-    private float cancelledFlightNumber;
-    private float maxDelay;
-    private final int flightsNumber;
 
-    public FlightsDelay() {
 
-    }
 }
