8ea564d3abd7ac0f81fd4926d4ba1156184f490e
not-Whale <rezepinn@yandex.ru>
2021-12-18 01:04:22 +0300

    Scripted auto-commit on change (2021-12-18 01:04:22) by gitwatch.sh

--- Development of Parallel and Distributed Programs/lab_3/src/main/java/FlightsDelay.java
+++ Development of Parallel and Distributed Programs/lab_3/src/main/java/FlightsDelay.java
@@ -3,13 +3,27 @@ import java.io.Serializable;
 public class FlightsDelay implements Serializable {
     private static final int DEPARTURE_AIRPORT = 11;
     private static final int DESTINATION_AIRPORT = 14;
-    private static final int DALEY_DURATION = 18;
+    private static final int DELAY_DURATION = 18;
     private static final int CANCELLED_STATUS = 19;
 
     private boolean cancelled;
     private float delay;
 
     public FlightsDelay(String[] flightData) {
-        
+        String cancelledStatusString = flightData[CANCELLED_STATUS];
+        float cancelledStatus = Float.parseFloat(cancelledStatusString);
+
+        if (cancelledStatus == 0.0) {
+            this.cancelled = false;
+            this.delay = Float.parseFloat(flightData[DELAY_DURATION]);
+        } else {
+            this.cancelled = true;
+        }
     }
+
+    public boolean getCancelledStatus() { return this.cancelled; }
+
+    public float getDelay() { return this.delay; }
+
+    
 }
