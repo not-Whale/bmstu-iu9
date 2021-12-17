d3cd66f7dacf34e2183f369ba47488226da8b558
not-Whale <rezepinn@yandex.ru>
2021-12-18 02:00:29 +0300

    Scripted auto-commit on change (2021-12-18 02:00:29) by gitwatch.sh

--- Development of Parallel and Distributed Programs/lab_3/src/main/java/AirportsFlightsStats.java
+++ Development of Parallel and Distributed Programs/lab_3/src/main/java/AirportsFlightsStats.java
@@ -1,3 +1,7 @@
-public class AirportsFlightsStats {
-    
+public class AirportsFlightsStats extends DelaysStats {
+    private final String departureAirportDescription;
+    private final String destinationAirportDescription;
+
+    public AirportsFlightsStats(Tuple2<String, String> airportDescriptions,
+                                DelaysStats delaysStats, Map<String, Sting> a)
 }
