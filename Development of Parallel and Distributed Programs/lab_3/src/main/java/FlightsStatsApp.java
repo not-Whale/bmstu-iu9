cf0a86227f22e39d0158981db7049e308a8ca504
not-Whale <rezepinn@yandex.ru>
2021-12-17 22:50:31 +0300

    Scripted auto-commit on change (2021-12-17 22:50:31) by gitwatch.sh

---
+++ Development of Parallel and Distributed Programs/lab_3/src/main/java/FlightsStatsApp.java
@@ -0,0 +1,14 @@
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+
+public class FlightsStatsApp {
+    private static final String flightsFilePath = "scr/main/resources/flights.csv";
+    private static final String airportsFilePath = "scr/main/resources/airports.csv";
+
+
+    public static void main(String[] args) throws Exception {
+        SparkConf conf = new SparkConf().setAppName("lab3");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+    }
+}
