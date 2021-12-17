3eba1843d17191df56bea63d4707b9c3df8e3108
not-Whale <rezepinn@yandex.ru>
2021-12-17 20:15:50 +0300

    Scripted auto-commit on change (2021-12-17 20:15:50) by gitwatch.sh

--- Development of Parallel and Distributed Programs/lab_2/src/main/java/FlightsStatsApp.java
+++ Development of Parallel and Distributed Programs/lab_2/src/main/java/FlightsStatsApp.java
@@ -20,17 +20,16 @@ public class FlightsStatsApp {
 
         MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, FlightsMapper.class);
         MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, AirportsMapper.class);
-        FileOutputFormat.setOutputPath(job, new Path(args[2]));
 
+        FileOutputFormat.setOutputPath(job, new Path(args[2]));
         job.setMapOutputKeyClass(AnFWritableComparable.class);
-        job.setMapOutputValueClass(Text.class);
         job.setPartitionerClass(AirportsPartitioner.class);
         job.setGroupingComparatorClass(AirportsGroupingComparator.class);
         job.setReducerClass(AirportsReducer.class);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(Text.class);
-        job.setNumReduceTasks(2);
 
+        job.setNumReduceTasks(2);
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
