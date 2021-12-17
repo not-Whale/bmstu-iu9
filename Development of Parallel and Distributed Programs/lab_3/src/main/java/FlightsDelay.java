320dff22f300afa0edc45dfcd92cbbdade486f3f
not-Whale <rezepinn@yandex.ru>
2021-12-18 00:59:16 +0300

    Scripted auto-commit on change (2021-12-18 00:59:15) by gitwatch.sh

--- Development of Parallel and Distributed Programs/lab_3/src/main/java/FlightsDelay.java
+++ Development of Parallel and Distributed Programs/lab_3/src/main/java/FlightsDelay.java
@@ -3,6 +3,6 @@ import java.io.Serializable;
 public class FlightsDelay implements Serializable {
     private static final int DEPARTURE_AIRPORT = 11;
     private static final int DESTINATION_AIRPORT = 14;
-    private static final int CANCELLED_STATUS = 21;
-    
+    private static final int CANCELLED_STATUS = 19;
+
 }
