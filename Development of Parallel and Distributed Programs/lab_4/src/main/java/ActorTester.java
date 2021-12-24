297ff6bfa972e8afe62cbddcb9eee054ea44bd43
not-Whale <rezepinn@yandex.ru>
2021-12-25 01:20:52 +0300

    Scripted auto-commit on change (2021-12-25 01:20:52) by gitwatch.sh

--- Development of Parallel and Distributed Programs/lab_4/src/main/java/ActorTester.java
+++ Development of Parallel and Distributed Programs/lab_4/src/main/java/ActorTester.java
@@ -1,2 +1,8 @@
-public class ActorTester {
+import akka.actor.AbstractActor;
+
+public class ActorTester extends AbstractActor {
+    @Override
+    public Receive createReceive() {
+        return null;
+    }
 }
