83edca7bf3b3ac31a5afeb113efe08ef01b2808f
not-Whale <rezepinn@yandex.ru>
2021-12-25 01:27:35 +0300

    Scripted auto-commit on change (2021-12-25 01:27:35) by gitwatch.sh

--- Development of Parallel and Distributed Programs/lab_4/src/main/java/ActorTester.java
+++ Development of Parallel and Distributed Programs/lab_4/src/main/java/ActorTester.java
@@ -1,10 +1,43 @@
 import akka.actor.AbstractActor;
 
 public class ActorTester extends AbstractActor {
+    private static final String PASSED_STATUS = "PASSED";
+    private static final String FAILED_STATUS = "FAILED";
+    private static final String CRASHED_STATUS = "CRASHED";
+
     @Override
     public Receive createReceive() {
         return null;
     }
 
-    
+    static class MessageStoreTestResult {
+        private final String packageID;
+        private final TestResult result;
+
+        public MessageStoreTestResult(
+                String packageID,
+                String status,
+                String testName,
+                String expectedResult,
+                String receivedResult) {
+            this.packageID = packageID;
+            this.result = new TestResult(status, testName, expectedResult, receivedResult);
+        }
+
+        public String getPackageID() {
+            return packageID;
+        }
+
+        public TestResult getResult() {
+            return result;
+        }
+
+        @Override
+        public toString() {
+            return "Package ID: "
+                    + getPackageID()
+                    + "\nResult: "
+                    + getResult();
+        }
+    }
 }
