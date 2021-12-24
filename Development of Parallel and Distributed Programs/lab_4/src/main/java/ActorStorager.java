28ef703e5592cb2984ce82dcb5631281dc8640ab
not-Whale <rezepinn@yandex.ru>
2021-12-25 01:02:06 +0300

    Scripted auto-commit on change (2021-12-25 01:02:06) by gitwatch.sh

--- Development of Parallel and Distributed Programs/lab_4/src/main/java/ActorStorager.java
+++ Development of Parallel and Distributed Programs/lab_4/src/main/java/ActorStorager.java
@@ -1,2 +1,12 @@
-public class ActorStorager {
+import akka.actor.AbstractActor;
+
+import java.util.HashMap;
+
+public class ActorStorager extends AbstractActor {
+    private final Map<String, List<TestResult>> results = new HashMap<>();
+
+    @Override
+    public Receive createReceive() {
+        return null;
+    }
 }
