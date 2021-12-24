147590a97609deaf543cb9459ce0aaf06c024268
not-Whale <rezepinn@yandex.ru>
2021-12-24 23:44:00 +0300

    Scripted auto-commit on change (2021-12-24 23:44:00) by gitwatch.sh

--- Development of Parallel and Distributed Programs/lab_4/src/main/java/JSTester.java
+++ Development of Parallel and Distributed Programs/lab_4/src/main/java/JSTester.java
@@ -1,9 +1,14 @@
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+
 import java.io.IOException;
 
 public class JSTester {
     private static final String ACTOR_SYSTEM_NAME = "js_tester";
 
     public static void main(String[] args) throws IOException {
-        
+        ActorSystem actorSystem = ActorSystem.create(ACTOR_SYSTEM_NAME);
+        ActorRef actorRouter = actorSystem.actorOf(Props.create(ActorRouter.class));
+
     }
 }
