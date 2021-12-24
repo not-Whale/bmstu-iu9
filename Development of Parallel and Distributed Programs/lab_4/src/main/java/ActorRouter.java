76232e7427b4ceb06d5a18cdccbd1e0a467ebc76
not-Whale <rezepinn@yandex.ru>
2021-12-25 00:42:17 +0300

    Scripted auto-commit on change (2021-12-25 00:42:17) by gitwatch.sh

--- Development of Parallel and Distributed Programs/lab_4/src/main/java/ActorRouter.java
+++ Development of Parallel and Distributed Programs/lab_4/src/main/java/ActorRouter.java
@@ -1,2 +1,16 @@
-public class ActorRouter {
+import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
+import akka.routing.Router;
+
+public class ActorRouter extends AbstractActor {
+    private static final int TESTERS_AMOUNT = 10;
+
+    private final ActorRef storager;
+    private final Router router;
+
+
+    @Override
+    public Receive createReceive() {
+        return null;
+    }
 }
