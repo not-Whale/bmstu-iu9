63ac821470f458761f6ebac0fcf65aa8cf883c44
not-Whale <rezepinn@yandex.ru>
2021-12-25 02:53:48 +0300

    Scripted auto-commit on change (2021-12-25 02:53:48) by gitwatch.sh

---
+++ Development of Parallel and Distributed Programs/lab_4/post.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+{
+"packageId":"11",
+"jsScript":"var divideFn = function(a,b) { return a/b}  ",
+"functionName":"divideFn",
+"tests": [
+	{"testName":"test1",
+	 "expectedResult":"2.0",
+	 "params":[2,1]
+	},
+	{"testName":"test2",	 
+	 "expectedResult":"2.0",
+	 "params":[4,2]
+	}
+]
+}
