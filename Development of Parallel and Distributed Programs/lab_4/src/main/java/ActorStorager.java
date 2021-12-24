280256e6162ac9ff1150fa6f3fed68b4ef8016ab
not-Whale <rezepinn@yandex.ru>
2021-12-25 01:05:37 +0300

    Scripted auto-commit on change (2021-12-25 01:05:37) by gitwatch.sh

--- Development of Parallel and Distributed Programs/lab_4/src/main/java/ActorStorager.java
+++ Development of Parallel and Distributed Programs/lab_4/src/main/java/ActorStorager.java
@@ -1,6 +1,8 @@
 import akka.actor.AbstractActor;
 
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class ActorStorager extends AbstractActor {
     private final Map<String, List<TestResult>> results = new HashMap<>();
--- Development of Parallel and Distributed Programs/lab_4/src/main/java/TestResult.java
+++ Development of Parallel and Distributed Programs/lab_4/src/main/java/TestResult.java
@@ -1,2 +1,20 @@
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class TestResult {
+    private final String status;
+    private final String testName;
+    private final String expectedResult;
+    private final String receivedResult;
+
+    @JsonProperty
+    public TestResult(
+            @JsonProperty("status") String status,
+            @JsonProperty("testName") String testName,
+            @JsonProperty("expectedResult") String expectedResult,
+            @JsonProperty("receivedResult") String receivedResult) {
+        this.status = status;
+        this.testName = testName;
+        this.expectedResult = expectedResult;
+        this.receivedResult = receivedResult;
+    }
 }
