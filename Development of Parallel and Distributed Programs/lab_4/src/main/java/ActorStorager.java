69099b5dd0aaa8b0bf33815311735a32d639e3b9
not-Whale <rezepinn@yandex.ru>
2021-12-25 01:14:48 +0300

    Scripted auto-commit on change (2021-12-25 01:14:48) by gitwatch.sh

--- Development of Parallel and Distributed Programs/lab_4/src/main/java/ActorStorager.java
+++ Development of Parallel and Distributed Programs/lab_4/src/main/java/ActorStorager.java
@@ -1,4 +1,6 @@
 import akka.actor.AbstractActor;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.HashMap;
 import java.util.List;
@@ -10,5 +12,26 @@ public class ActorStorager extends AbstractActor {
     @Override
     public Receive createReceive() {
         return null;
+    }
+
+    static class MessageReturnResults {
+        private final String packageID;
+        private final List<TestResult> results;
+
+        @JsonCreator
+        public MessageReturnResults(
+                @JsonProperty("packageID") String packageID,
+                @JsonProperty("results") List<TestResult> results) {
+            this.packageID = packageID;
+            this.results = results;
+        }
+
+        public String getPackageID() {
+            return packageID;
+        }
+
+        public List<TestResult> getResults() {
+            return results;
+        }
     }
 }
