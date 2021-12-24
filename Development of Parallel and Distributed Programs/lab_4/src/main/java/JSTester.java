899aa39c3bf0b22a34ad40c89b241f1d3841a4d0
not-Whale <rezepinn@yandex.ru>
2021-12-24 23:54:50 +0300

    Scripted auto-commit on change (2021-12-24 23:54:50) by gitwatch.sh

--- Development of Parallel and Distributed Programs/lab_4/src/main/java/JSTester.java
+++ Development of Parallel and Distributed Programs/lab_4/src/main/java/JSTester.java
@@ -1,12 +1,18 @@
+import akka.NotUsed;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
 import akka.http.javadsl.Http;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
+import akka.http.javadsl.server.AllDirectives;
+import akka.http.javadsl.server.Route;
 import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
 
 import java.io.IOException;
 
-public class JSTester {
+public class JSTester extends AllDirectives {
     private static final String ACTOR_SYSTEM_NAME = "js_tester";
 
     public static void main(String[] args) throws IOException {
@@ -15,6 +21,13 @@ public class JSTester {
 
         final Http http = Http.get(actorSystem);
         final ActorMaterializer actorMaterializer = ActorMaterializer.create(actorSystem);
-        
+
+        JSTester instance = new JSTester();
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow =
+                instance.createRoute(actorRouter).flow(actorSystem, actorMaterializer);
+    }
+
+    private Route createRoute(ActorRef actorRouter) {
+        return
     }
 }
