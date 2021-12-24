24f7e0274409a7747a61008de150706443ba4168
not-Whale <rezepinn@yandex.ru>
2021-12-24 23:42:49 +0300

    Scripted auto-commit on change (2021-12-24 23:42:49) by gitwatch.sh

--- Development of Parallel and Distributed Programs/lab_4/src/main/java/JSTester.java
+++ Development of Parallel and Distributed Programs/lab_4/src/main/java/JSTester.java
@@ -1,5 +1,9 @@
+import java.io.IOException;
+
 public class JSTester {
-    public static void main(String[] args) {
+    private static final String ACTOR_SYSTEM_NAME = "js_tester";
+
+    public static void main(String[] args) throws IOException {
         
     }
 }
