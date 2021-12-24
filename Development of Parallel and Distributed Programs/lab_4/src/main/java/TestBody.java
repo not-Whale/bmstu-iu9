425d68432afc3ee77671c07c309406b864bd4be1
not-Whale <rezepinn@yandex.ru>
2021-12-25 00:33:33 +0300

    Scripted auto-commit on change (2021-12-25 00:33:33) by gitwatch.sh

--- Development of Parallel and Distributed Programs/lab_4/src/main/java/TestBody.java
+++ Development of Parallel and Distributed Programs/lab_4/src/main/java/TestBody.java
@@ -1,3 +1,16 @@
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class TestBody {
-    
+    private final String testName;
+    private final Object[] parameters;
+    private final String expectedResult;
+
+    @JsonCreator
+    public TestBody(
+            @JsonProperty("testName") String testName,
+            @JsonProperty("parameters") Object[] parameters,
+            @JsonProperty("expectedResult") String expectedResult) {
+        
+    }
 }
