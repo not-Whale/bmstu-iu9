626644b69a5e0c37680f2163f6b299366224e4dc
not-Whale <rezepinn@yandex.ru>
2021-12-25 05:28:12 +0300

    Scripted auto-commit on change (2021-12-25 05:28:12) by gitwatch.sh

--- Development of Parallel and Distributed Programs/lab_5/src/main/java/ResponseTimeAnalyser.java
+++ Development of Parallel and Distributed Programs/lab_5/src/main/java/ResponseTimeAnalyser.java
@@ -59,22 +59,14 @@ public class ResponseTimeAnalyser {
                     String url = query.get(QUERY_PARAMETER_URL).get();
                     int count = Integer.parseInt(query.get(QUERY_PARAMETER_COUNT).get());
                     return new Pair<>(url, count);
-                }).mapAsync(
-                        1, req -> {
-                    Patterns.ask(
-                            actorSystem,
-                            new MessageGetResult(req.first()),
-                            java.time.Duration.ofMillis(5000)
-                    ).thenCompose(
-                            result -> {
+                }).mapAsync(1, req -> {
+                    Patterns.ask(actorSystem, new MessageGetResult(req.first()), java.time.Duration.ofMillis(5000))
+                            .thenCompose(result -> {
                                 if (((Optional<Long>)result).isPresent()) {
                                     return CompletableFuture.completedFuture(new Pair<>(request.first(), ((Optional<Long>) result).get()));
                                 } else {
                                     Sink<Integer, CompletionStage<Long>> fold = Sink.fold(0L, (Function2<Long, Integer, Long>) Long::sum);
-                                    Sink<Pair<String, Integer>, CompletionStage<Long>> sink = Flow
-                                            .<Pair<String, Integer>>create()
-                                            .mapConcat(r -> new ArrayList<>((Collections.nCopies(r.second(), r.first()))))
-                                            .mapAsync(req.second(), url -> {
+                                    Sink<Pair<String, Integer>, CompletionStage<Long>> sink = Flow.<Pair<String, Integer>>create().mapConcat(r -> new ArrayList<>((Collections.nCopies(r.second(), r.first())))).mapAsync(req.second(), url -> {
                                                 long start = System.currentTimeMillis();
                                                 Request request1 = Dsl.get(url).build();
                                                 CompletableFuture<Response> whenResponse = Dsl.asyncHttpClient().executeRequest(request1).toCompletableFuture();
@@ -83,11 +75,9 @@ public class ResponseTimeAnalyser {
                                                     return CompletableFuture.completedFuture(duration);
                                                 });
                                             }).toMat(fold, Keep.right());
-                                    return Source.from(Collections.singletonList(req))
-                                            .toMat(sink, Keep.right())
-                                            .run(actorMaterializer)
-                                            .thenApply(sum -> new Pair<>(req.first(), sum / req.second()));
-                                }
-                            })})
+                                    return Source.from(Collections.singletonList(req)).toMat(sink, Keep.right()).run(actorMaterializer).thenApply(sum -> new Pair<>(req.first(), sum / req.second()));
+                                }}
+                            )
+                })
     }
 }
