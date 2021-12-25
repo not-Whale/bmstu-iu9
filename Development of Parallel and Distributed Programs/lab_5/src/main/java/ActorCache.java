8f0c52480ce769043b8be3cfd04cec951bbd0775
not-Whale <rezepinn@yandex.ru>
2021-12-25 05:57:12 +0300

    Scripted auto-commit on change (2021-12-25 05:57:12) by gitwatch.sh

--- Development of Parallel and Distributed Programs/lab_5/src/main/java/ActorCache.java
+++ Development of Parallel and Distributed Programs/lab_5/src/main/java/ActorCache.java
@@ -1,3 +1,14 @@
-public class ActorCache {
-    private 
+import akka.actor.AbstractActor;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ActorCache extends AbstractActor {
+    private final Map<String, Long> results = new HashMap<>();
+
+
+    @Override
+    public Receive createReceive() {
+        return null;
+    }
 }
