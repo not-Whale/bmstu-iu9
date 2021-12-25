e62a25f58808f5ecdfd4fcb27eac406b91d8758b
not-Whale <rezepinn@yandex.ru>
2021-12-25 04:54:51 +0300

    Scripted auto-commit on change (2021-12-25 04:54:51) by gitwatch.sh

---
+++ Development of Parallel and Distributed Programs/lab_5/src/main/java/ResponseTimeAnalyser.java
@@ -0,0 +1,32 @@
+import akka.NotUsed;
+import akka.actor.ActorSystem;
+import akka.http.javadsl.ConnectHttp;
+import akka.http.javadsl.Http;
+import akka.http.javadsl.ServerBinding;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
+import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
+
+import java.io.IOException;
+import java.util.concurrent.CompletionStage;
+
+public class ResponseTimeAnalyser {
+    public static void main(String[] args) throws IOException {
+        System.out.println("start!");
+        ActorSystem system = ActorSystem.create("routes");
+        final Http http = Http.get(system);
+        final ActorMaterializer materializer = ActorMaterializer.create(system);
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = <вызов метода которому передаем Http, ActorSystem и ActorMaterializer>;
+        final CompletionStage<ServerBinding> binding = http.bindAndHandle(
+                routeFlow,
+                ConnectHttp.toHost("localhost", 8080),
+                materializer
+        );
+        System.out.println("Server online at http://localhost:8080/\nPress RETURN to stop...");
+        System.in.read();
+        binding
+                .thenCompose(ServerBinding::unbind)
+                .thenAccept(unbound -> system.terminate()); // and shutdown when done
+    }
+}
