23e4b967a79f7a5e022157205601ca24ffa67bbf
not-Whale <rezepinn@yandex.ru>
2021-12-25 03:10:38 +0300

    Scripted auto-commit on change (2021-12-25 03:10:38) by gitwatch.sh

--- Development of Parallel and Distributed Programs/lab_4/post.sh
+++ Development of Parallel and Distributed Programs/lab_4/post.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -H { 'Content-Type: application/json' --data '{"packageId":"11", "jsScript": "var divideFn = function(a,b) {return a/b}", "functionName": "divideFn", "tests": [ {"testName": "test1", "expectedResult": "2.0", "params": [2,1] }, {"testName": "test2", "expectedResult": "2.0", "params": [4,2] } ] }' http://localhost:8000
+curl -H { 'Content-Type: application/json' --data '{"packageId":"11", "jsScript":"var divideFn = function(a,b){return a/b}", "functionName":"divideFn", "tests":[{"testName":"test1", "expectedResult":"2.0", "params":[2,1]}, {"testName":"test2", "expectedResult":"2.0", "params":[4,2]}]}' http://localhost:8080
