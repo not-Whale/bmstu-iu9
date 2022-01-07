81cd6e1e61710a355d32ad400707a2c6326b9120
not-Whale <rezepinn@yandex.ru>
2022-01-07 08:15:26 +0300

    Scripted auto-commit on change (2022-01-07 08:15:26) by gitwatch.sh

--- Development of Parallel and Distributed Programs/lab_1/run.sh
+++ Development of Parallel and Distributed Programs/lab_1/run.sh
@@ -18,6 +18,8 @@ hdfs dfs -rmr hdfs://localhost:9000/user/lopata/output_lab_1
 
 hadoop jar ~/hadoop-2.10.1/share/hadoop/mapreduce/hadoop-mapreduce-examples-2.10.1.jar pi 2 5
 
+rm -rf output_lab_1
+
 mvn package
 hadoop fs -copyFromLocal src/main/resourses/warandpeace1.txt
 export HADOOP_CLASSPATH=target/hadoop-examples-1.0-SNAPSHOT.jar
