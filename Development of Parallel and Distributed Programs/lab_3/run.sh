9ebee2bd18b982252ffb08ba5017837db74c2c35
not-Whale <rezepinn@yandex.ru>
2022-01-07 08:22:23 +0300

    Scripted auto-commit on change (2022-01-07 08:22:22) by gitwatch.sh

--- Development of Parallel and Distributed Programs/lab_3/run.sh
+++ Development of Parallel and Distributed Programs/lab_3/run.sh
@@ -14,15 +14,15 @@ hdfs namenode -format
 start-dfs.sh
 start-yarn.sh
 
-hdfs dfs -rmr hdfs://localhost:9000/user/lopata/output_lab_2
+hdfs dfs -rmr hdfs://localhost:9000/user/lopata/output_lab_3
 
 hadoop jar ~/hadoop-2.10.1/share/hadoop/mapreduce/hadoop-mapreduce-examples-2.10.1.jar pi 2 5
 
-rm -rf output_lab_2
+rm -rf output_lab_3
 
 mvn package
 hadoop fs -copyFromLocal src/main/resourses/airports.csv
 hadoop fs -copyFromLocal src/main/resourses/flights.csv
-export HADOOP_CLASSPATH=target/hadoop-examples-1.0-SNAPSHOT.jar
+export HADOOP_CLASSPATH=target/spark-examples-1.0-SNAPSHOT.jar
 hadoop FlightsStatsApp flights.csv airports.csv output_lab_2
 hadoop fs -copyToLocal output_lab_2
