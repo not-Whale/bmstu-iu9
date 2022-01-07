ea40ba46385e77a1689472ecc130ee67a7253fb0
not-Whale <rezepinn@yandex.ru>
2022-01-07 08:16:35 +0300

    Scripted auto-commit on change (2022-01-07 08:16:35) by gitwatch.sh

---
+++ Development of Parallel and Distributed Programs/lab_2/run.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+stop-yarn.sh
+stop-dfs.sh
+
+rm -rf ~/hdata/datanode
+rm -rf ~/hdata/namenode
+
+mkdir ~/hdata/datanode
+mkdir ~/hdata/namenode
+
+hdfs namenode -format
+
+start-dfs.sh
+start-yarn.sh
+
+hdfs dfs -rmr hdfs://localhost:9000/user/lopata/output_lab_1
+
+hadoop jar ~/hadoop-2.10.1/share/hadoop/mapreduce/hadoop-mapreduce-examples-2.10.1.jar pi 2 5
+
+rm -rf output_lab_1
+
+mvn package
+hadoop fs -copyFromLocal src/main/resourses/warandpeace1.txt
+export HADOOP_CLASSPATH=target/hadoop-examples-1.0-SNAPSHOT.jar
+hadoop WordCountApp warandpeace1.txt output_lab_1
+hadoop fs -copyToLocal output_lab_1
